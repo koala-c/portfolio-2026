05013da2ace44854f4d7afa9a0285392b03c7d6b
Koala Code <k0a1ac0d3@gmail.com>
2026-02-06 16:01:32 +0100

    Update THREE.js import to use a specific version from unpkg

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x0b0b0f);
